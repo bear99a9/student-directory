d99fc1711d4ac0c1e2a7222dd6e03f4929c0e825
Sean Edwards <seantrevoredwards@gmail.com>
2020-12-10 10:43:14 +0000

    step 8 exercise 6 added .center() to output

--- directory.rb
+++ directory.rb
@@ -1,22 +1,22 @@
 def input_students
-  puts "Please enter the names of the student"
-  puts "To finish, jut hit return twice"
+  puts "Please enter the names of the student".center(120)
+  puts "To finish, jut hit return twice".center(120)
   #create an empty array
   students = []
   # get the first name
   name = gets.chomp
   # ask for age
-  puts "Please enter the age of the student"
-  puts "To finish, jut hit return twice"
+  puts "Please enter the age of the student".center(120)
+  puts "To finish, jut hit return twice".center(120)
   # get the age
   age = gets.chomp
   # until the name & age are empty, repeat this code
   until name.empty? && age.empty? do
     #add the student hash to the array
     students << {name: name, age: age, cohort: :november}
-    puts "Now we have #{students.count} students"
-    puts "Please enter the name and then age of the student"
-    puts "To finish, jut hit return twice"
+    puts "Now we have #{students.count} students".center(120)
+    puts "Please enter the name and then age of the student".center(120)
+    puts "To finish, jut hit return twice".center(120)
     # get another name from the user
     name = gets.chomp
     age = gets.chomp
@@ -26,18 +26,18 @@ def input_students
 end
 
 def print_header
-  puts "The students of Villains Academy"
-  puts "-------------"
+  puts "The students of Villains Academy".center(120)
+  puts "-------------".center(120)
 end
 
 def print(students)
   students.each_with_index do |student, index|
-    puts "#{index+1}. #{student[:name]} #{student[:age]} #{student[:cohort].capitalize} cohort"
+    puts "#{index+1}. #{student[:name]} #{student[:age]} #{student[:cohort].capitalize} cohort".center(120)
   end
 end
 
 def print_footer(students)
-  puts "Overall, we have #{students.count} great students"
+  puts "Overall, we have #{students.count} great students".center(120)
 end
 
 students = input_students
