1be34c27d3b22d4f768382a099b4f7ec7de88263
Sean Edwards <seantrevoredwards@gmail.com>
2020-12-11 12:01:01 +0000

    step 9 updated method interactive_menu now functions also hcnaged some of the user interface print out to make it more user friendly.

--- directory.rb
+++ directory.rb
@@ -1,9 +1,28 @@
 def interactive_menu
+  students = []
   loop do
-  # 1. print the menu and ask the user what to do
-  # 2. read the input and save it into a variable
-  # 3. do what the user has asked
-  # 4. repeat from step 1
+    # 1. print the menu and ask the user what to do
+    puts "1. Input the students"
+    puts "2. Show the students"
+    puts "9 Exit" # 9 because I've got to add more items
+    # 2. read the input and save it into a variable
+    selection = gets.chomp
+    # 3. do what the user has asked
+    case selection
+    when "1"
+      # input the students
+      students = input_students("No name added", "No age added", "Unknown")
+    when "2"
+      # show the students
+      print_header(students)
+      print_student_by_cohort(students)
+      print_footer(students)
+    when "9"
+      exit # this will cause the program to terminate
+    else
+      puts "I don't know what you meant, try again"
+      # 4. repeat from step 1
+    end
   end
 end
 
@@ -15,12 +34,12 @@ def input_students(default_name, default_age, default_cohort)
     # ask for name of student
     puts "Please enter the name of the student".center(120)
     puts "Enter 'typo' to re-enter the students data".center(120)
-    puts "TO FINISH AT ANY TIME, ENTER 'exit'".center(120)
+    puts "TO FINISH INPUTTING STUDENT DATA, ENTER 'stop'".center(120)
     # get the first name
     name = gets.strip
     name = default_name if name.empty?
     next if name == "typo"
-    break if name == "exit"
+    break if name == "stop"
     # ask for age
     puts "Please enter the age of the student".center(120)
     puts "Enter 'typo' to re-enter the students data".center(120)
@@ -28,7 +47,7 @@ def input_students(default_name, default_age, default_cohort)
     age = gets.strip
     age = default_age if age.empty?
     next if age == "typo"
-    break if age == "exit"
+    break if age == "stop"
     # ask for the cohort
     puts "Please enter the cohort of the student".center(120)
     puts "Enter 'typo' to re-enter the students data".center(120)
@@ -36,7 +55,7 @@ def input_students(default_name, default_age, default_cohort)
     student_cohort = gets.strip
     student_cohort = default_cohort if student_cohort.empty?
     next if student_cohort == "typo"
-    break if student_cohort == "exit"
+    break if student_cohort == "stop"
     #add the student hash to the array
     students << {name: name, age: age, cohort: student_cohort.to_sym}
     # prints out the student total
@@ -75,9 +94,11 @@ def print_footer(students)
   puts "The list is empty as no data was added by user!".center(120) if students.empty?
   puts "Overall, we have #{students.count} great student#{'s' if students.count > 1}!".center(120) if !students.empty?
 end
-# variable for students info with default parameters entered
-students = input_students("No name added", "No age added", "Unknown")
-# nothing happens until we call the methods
-print_header(students)
-print_student_by_cohort(students)
-print_footer(students)
+# # variable for students info with default parameters entered
+# students = input_students("No name added", "No age added", "Unknown")
+# # nothing happens until we call the methods
+# print_header(students)
+# #print_student_by_index(students)
+# print_student_by_cohort(students)
+# print_footer(students)
+interactive_menu
